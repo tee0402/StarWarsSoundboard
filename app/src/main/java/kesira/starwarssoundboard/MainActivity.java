bd0f975586a20c6e777d364f095e5c40cef1a6bc
Iao Seng Sio <tonysio42@gmail.com>
2017-03-25 17:33:34 -1000

    Added tabs

--- app/src/main/java/kesira/starwarssoundboard/MainActivity.java
+++ app/src/main/java/kesira/starwarssoundboard/MainActivity.java
@@ -1,11 +1,17 @@
 package kesira.starwarssoundboard;
 
-import android.app.ActionBar;
 import android.media.MediaPlayer;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +20,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     public void playSound(View v) {
         Button b = (Button) v;
         String sound = b.getTag().toString();
         MediaPlayer mp = MediaPlayer.create(this, getResources().getIdentifier(sound,"raw",getPackageName()));
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
         mp.start();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Anakin vs. Obi-Wan");
+        adapter.addFragment(new TwoFragment(), "Palpatine Reveals Himself");
+        adapter.addFragment(new ThreeFragment(), "Tragedy of Darth Plagueis");
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final ArrayList<Fragment> mFragmentList = new ArrayList<>();
+        private final ArrayList<String> mFragmentTitleList = new ArrayList<>();
+
+        private ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        private void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+        
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
