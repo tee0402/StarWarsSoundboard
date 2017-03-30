0ed3f8054a64c765cc2e20dcc101015fa964b6dd
Iao Seng Sio <tonysio42@gmail.com>
2017-03-29 19:55:46 -1000

    Added Anakin vs. Obi-Wan finale

--- app/src/main/java/kesira/starwarssoundboard/SixFragment.java
+++ app/src/main/java/kesira/starwarssoundboard/SixFragment.java
@@ -21,7 +21,7 @@ public class SixFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_six, container, false);
-        for (int i = 1; i <= 22; i++) {
+        for (int i = 1; i <= 33; i++) {
             Button b = (Button) v.findViewById((getResources().getIdentifier("button" + i, "id",
                     v.getContext().getPackageName())));
             registerForContextMenu(b);
